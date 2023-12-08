b3cd0d7f853d4eecb124c09f65ff69f52a96d027
malfaro21 <malfaro21@students.desu.edu>
2023-12-08 08:49:15 +0000

    extract offset

--- tranlation.c
+++ tranlation.c
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 
 int page_number(int logical_address);
+int extract_offset(int ogical_address);
 
 
 int extract_pagenum(int logical_address){
     int page_number = (logical_address >> 8) & 0x00FF;
 }
+int extract_offset(int logical_address){
+    int Offset = logical_address & 0x00FF;
+}
+
