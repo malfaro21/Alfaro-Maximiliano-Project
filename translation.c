c6217b943e1644be1ea19184054c441602334924
malfaro21 <malfaro21@students.desu.edu>
2023-12-09 21:17:14 +0000

    .

--- translation.c
+++ translation.c
@@ -10,9 +10,8 @@ int extract_offset(int logical_address);
 int translate_address(int logical_address);
 
 
-int extract_pagenum(int logical_address){
+int extract_page(int logical_address){
     int page_number = (logical_address >> 8) & 0x00FF;
-}
-int extract_offset(int logical_address){
     int Offset = logical_address & 0x00FF;
 }
+
