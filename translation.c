f3acfe41975e5925348b8a5b0b4d9070123a03a8
malfaro21 <malfaro21@students.desu.edu>
2023-12-09 21:12:20 +0000

    defined frame size and page size.

--- translation.c
+++ translation.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define Frame_size 256 //frame size is 256 frames.
+#define Page_size 256 // page size of 2^8 bytes.
+
 int page_number(int logical_address);
 int extract_offset(int logical_address);
 int translate_address(int logical_address);
