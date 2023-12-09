66373bbe752dc360f92f548160a6cc9eef055a73
malfaro21 <malfaro21@students.desu.edu>
2023-12-09 21:13:51 +0000

    defined TLB entries

--- translation.c
+++ translation.c
@@ -3,6 +3,7 @@
 
 #define Frame_size 256 //frame size is 256 frames.
 #define Page_size 256 // page size of 2^8 bytes.
+#define TLB_entries 16 //16 entries in the TLB
 
 int page_number(int logical_address);
 int extract_offset(int logical_address);
