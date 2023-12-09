964aef2c57fb337b2c2c516706b4a3376581a5ed
jdmunoz21 <103907161+jdmunoz21@users.noreply.github.com>
2023-12-09 16:26:22 -0500

    defined physical memory size

--- translation.c
+++ translation.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define Frame_size 256 //frame size is 256 frames.
-#define Page_size 256 // page size of 2^8 bytes.
-#define TLB_entries 16 //16 entries in the TLB
+#define FRAME_SIZE 256 // frame size is 256 frames.
+#define PAGE_SIZE 256 // page size of 2^8 bytes.
+#define TLB_SIZE 16 // 16 entries in the TLB
+#define MEM_SIZE 65336 // physical memory is 65336 bytes
 
 int page_number(int logical_address);
 int extract_offset(int logical_address);
 int translate_address(int logical_address);
 
-
 int extract_page(int logical_address){
     int page_number = (logical_address >> 8) & 0x00FF;
-    int Offset = logical_address & 0x00FF;
+    int offset = logical_address & 0x00FF;
 }
 
