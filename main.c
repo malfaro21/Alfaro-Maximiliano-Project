ba16640a1f42642f37ee7c0d4e6a071dca4ab734
malfaro21 <malfaro21@students.desu.edu>
2023-12-12 17:34:20 +0000

    Opening addresses file and priting logical_address

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+
+int physical_Memory[256 * 256];
+int logical_address;
+
+FILE *address_file;
+char address[7]; //Store address
+
+int main(){
+    address_file = fopen("addresses.txt", "r");
+
+    while (fgets(address, 7, address_file) != NULL){
+        logical_address = atoi(address); // convert char array to int
+        printf("logical address: %d\n", logical_address);
+    }
+    
+}
