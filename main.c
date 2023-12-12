53cc07e877d1c41b238bac1ba802b5f19f9a0020
jdmunoz21 <jdmunoz21@students.desu.edu>
2023-12-12 17:17:08 +0000

    main.c

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int physical_Memory[256*256];
+signed char value;
+int logical_address;
+
+FILE *address_file;
+char address[7];
+
+int main(){
+    address_file = fopen("addresses.txt", "r");
+    while (fgets(address, 7, address_file) != NULL){
+        logical_address = atoi(address);
+        printf("Logical address: %d\n", logical_address);
+    }
+}
+
 
