a280529512475d4d51f30f72dc6c680414d39195
cibautista21@students.desu.edu <cibautista21@students.desu.edu>
2023-12-13 03:47:01 +0000

    tlb hit function

--- TLB.c
+++ TLB.c
@@ -20,11 +20,16 @@ void checktlb(int page){
     for(int i = 0; i < TLB_SIZE; i++){
         if(TLB[i].page_number == page){ //check if page is there
             tlbhitcount++; //add to hit count
-            //TODO TLB hit
+            tlbhit(TLB[i].frame_number);
         }
         else{
             tlbmisscount++; //add to miss count
             //TODO checkpage_table
         }
     }
+}
+
+void tlbhit(int frame_number){
+    value = physical_Memory[(frame_number * 256) + offset];
+    physical_address = (frame_number * 256) + offset;
 }
--- main.c
+++ main.c
@@ -4,8 +4,9 @@
 #include "PhysicalMem.c"
 
 int logical_address;
-
+signed char value;
 FILE *address_file;
+int physical_address;
 char address[7]; //Store address
 
 int page_Number;
@@ -20,26 +21,6 @@ void getOffSet(int logical_address){
 
 int main(){
     address_file = fopen("addresses.txt", "r");
-
-    insert_Chunk(66);
-    signed char value = physical_Memory[20];
-    int physical_Address = 20;
-    printf("Address: %d Value: %d\n", physical_Address, value);
-
-    insert_Chunk(244);
-    value = physical_Memory[(256*1) + 29];
-    physical_Address = (256*1) + 29;
-    printf("Address: %d Value: %d\n", physical_Address, value);
-
-    insert_Chunk(117);
-    value = physical_Memory[(256*2) + 246];
-    physical_Address = (256*2) + 246;
-    printf("Address: %d Value: %d\n", physical_Address, value);
-
-    insert_Chunk(209);
-    value = physical_Memory[(256*3) + 179];
-    physical_Address = (256*3) + 179;
-    printf("Address: %d Value: %d\n", physical_Address, value);
 
     while (fgets(address, 7, address_file) != NULL){
         logical_address = atoi(address); // convert char array to int
