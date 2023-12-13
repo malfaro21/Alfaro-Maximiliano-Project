bb70f5b153822568705225f74a83f5a665974a9a
malfaro21 <malfaro21@students.desu.edu>
2023-12-13 03:22:39 +0000

    check tlb if page there

--- PhysicalMem.c
+++ PhysicalMem.c
@@ -3,7 +3,6 @@
 
 #define FRAME_SIZE 256 // frame size is 256 frames.
 #define PAGE_SIZE 256 // page size of 2^8 bytes.
-#define TLB_SIZE 16 // 16 entries in the TLB
 #define MEM_SIZE 65336 // physical memory is 65336 bytes
 #define PAGE_ENTRIES 256 // 2^8 entries in the page table.
 #define CHUNK 256 // amount of bytes we need to load into a frame
--- TLB.c
+++ TLB.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "main.c"
+#include "PhysicalMem.c"
+
+#define TLB_SIZE 16 // 16 entries in the TLB
 
 struct tlb_cell{
     int page_number;
     int frame_number;
 };
 
-struct tlb_cell TLB[16];
+struct tlb_cell TLB[TLB_SIZE];
 int tlbCounter = 0;
+int tlbhitcount = 0;
+int tlbmisscount = 0;
 
+void checktlb(int page){
+    for(int i = 0; i < TLB_SIZE; i++){
+        if(TLB[i].page_number == page){ //check if page is there
+            tlbhitcount++; //add to hit count
+            //TODO TLB hit
+        }
+        else{
+            tlbmisscount++; //add to miss count
+            //TODO checkpage_table
+        }
+    }
+}
